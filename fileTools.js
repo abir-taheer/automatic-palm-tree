ccf75a3bab421a4b5d157822ec1bfb7180e53d17
Abir Taheer <abir488@gmail.com>
2019-11-03 02:53:07 -0500

    added everything

---
+++ fileTools.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const childProcess = require("child_process");
+const homedir = require('os').homedir();
+
+const gitFindRecursive = (dir, do_not_search = ["node_modules", ".git"]) => {
+
+    if(dir.endsWith("/")) dir = dir.substr(0, dir.length - 1);
+
+    let git_repos = [];
+    try {
+        let outer_dirs = fs.readdirSync(dir,{ withFileTypes: true }).filter(dirent => dirent.isDirectory());
+
+        for(let x = 0; x < outer_dirs.length ; x++){
+            let current_dir = outer_dirs[x].name;
+            if(! do_not_search.includes(current_dir))
+                git_repos = [...git_repos, ...gitFindRecursive(`${dir}/${outer_dirs[x].name}`)];
+
+            if(current_dir === ".git")
+                git_repos.push(dir);
+        }
+    } catch(e){}
+
+    return git_repos;
+};
+
+const performGitPull = (dir) => {
+    let data = "";
+    try {
+        data = childProcess.execSync(`cd ${dir} ; git pull`, {timeout: 5000, stdio: "pipe"}).toString();
+    } catch (e) {
+        data = `There was an error. This is likely due to insufficient credentials being provided. Below is the error: \n\n${e}`;
+    }
+    return data;
+};
+
+const storeCredentials = (username, password, site) => {
+    fs.appendFileSync(`${homedir}/.git-credentials`, `https://${username}:${password}@${site}\n`);
+};
+
+const deleteCredentials = () => {
+    if(fs.existsSync(`${homedir}/.git-credentials`))
+        fs.unlinkSync(`${homedir}/.git-credentials`);
+};
+
+module.exports = {
+    gitFindRecursive,
+    performGitPull,
+    storeCredentials,
+    deleteCredentials
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+const bodyParser = require("body-parser");
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require('http').createServer(app);
+const fileTools = require("./fileTools");
+
+const app_port = process.env.PORT || 3448;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.route("/pull").post((req, res) => {
+    let dirs = fileTools.gitFindRecursive("./../../");
+    fileTools.storeCredentials(req.body["github-username"], req.body["github-password"], "github.com");
+    fileTools.storeCredentials(req.body["gitlab-username"], req.body["gitlab-password"], "gitlab.com");
+    let response = {};
+    dirs.map(i => response[path.resolve(i)] = fileTools.performGitPull(i));
+    fileTools.deleteCredentials();
+    res.json(response);
+});
+
+app.route("*").get((req,res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+
+http.listen(app_port, () => {
+    console.log('listening on *:' + app_port);
+});
